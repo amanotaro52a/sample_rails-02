9111cdb18ac1dddf4f477498af11b65268f6e97c
天野太郎 <amanotaro@amanotarounoMacBook-Air.local>
2025-10-20 17:52:32 +0900

    userのCRUD機能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,57 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+  def index
+    @users = User.all
+  end
+  def show
+  end
+  def edit
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user_url(@user), notice: "ユーザーの新規登録に成功しました" }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity}
+      end
+    end  
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: "ユーザー情報を更新しました", status: :see_other }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }  
+      end
+    end
+  end
+ 
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "ユーザー情報は削除されました" }
+      format.json { head :no_content }
+    end
+  end  
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end  
+end
